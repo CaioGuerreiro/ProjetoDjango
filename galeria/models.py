17e75b6a5ad0086a0d8e413bcadcdf77071ab6f8
CaioGuerreiro <78889968+CaioGuerreiro@users.noreply.github.com>
2024-01-17 11:28:18 -0300

    Projeto Django concluido

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class fotografia(models.Model):
+
+    opcoes_categoria = [("NEBULOSA", "nebulosa"), ("ESTRELA", "estrela"), ("GALAXIA", "galaxia"), ("PLANETA", "planeta"),]
+
+    nome = models.CharField(max_length = 100, null = False, blank = False)
+    legenda = models.CharField(max_length = 150, null = False, blank = False)
+    categoria = models.CharField(max_length = 100, choices = opcoes_categoria, default = '')
+    descricao = models.TextField(null = False, blank = False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d", blank=True)
+    publicada = models.BooleanField(default = False)
+    data_publicada = models.DateTimeField(default = datetime.now, blank = False)
+
+    def __str__(self):
+        return self.nome
--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import fotografia
+
+
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = fotografia.objects.order_by("data_publicada").filter(publicada = True)
+   
+    
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    Fotografia = get_object_or_404(fotografia, pk = foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": Fotografia})
+
+def buscar(request):
+    fotografias = fotografia.objects.order_by("data_publicada").filter(publicada = True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+
+    return render(request, "galeria/buscar.html", {"cards": fotografias})
